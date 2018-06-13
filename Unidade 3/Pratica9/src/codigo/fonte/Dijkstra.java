af25eeea1043b31446bdc114b3e52a83fb09deba
Victor Emanuel <victorers1@hotmail.com>
2018-06-13 10:22:46 -0300

    Pratica 9 testada

--- Unidade 3/Pratica9/src/codigo/fonte/Dijkstra.java
+++ Unidade 3/Pratica9/src/codigo/fonte/Dijkstra.java
@@ -27,6 +27,7 @@ public class Dijkstra {
         dist = new int[n];
         pred = new int[n];
         settled = new boolean[n];
+        naoacomodados = new PriorityQueue<>();
 
         for (int i = 0; i < dist.length; i++) {
             dist[i] = Integer.MAX_VALUE; // Distâncias para cada nó começa +inf
@@ -35,7 +36,7 @@ public class Dijkstra {
         }
         dist[source] = 0; // Distância do começo a si próprio é 0
         pred[source] = source; // Predecessor no começo é ele mesmo
-
+        naoacomodados.add(new Node(source, dist[source]));
     }
 
     // Atualizacao da distancia, da prioridade, e do predecessor de um no
@@ -43,8 +44,8 @@ public class Dijkstra {
     void update(int v, int x) { //Dado que estou em x, verifica o caminho pra v
         if (dist[v] > dist[x] + g.value(x, v)) {
             dist[v] = dist[x] + g.value(x, v); //value(x, y) == value(y, x)
+            pred[v] = x; // Atualização do predecessor
         }
-        pred[v] = x; // Atualização do predecessor
         naoacomodados.add(new Node(v, dist[v])); // S = S + {v}
     }
 
@@ -57,7 +58,7 @@ public class Dijkstra {
     int nextNode() {
         Node no;
         while (!naoacomodados.isEmpty()) {
-            no = naoacomodados.poll(); // poll() Retira e retorna o nó
+            no = naoacomodados.poll(); // poll() retira e retorna o nó
             if (!settled[no.id]) { // Se não for um nó acomodado
                 return no.id;
             }
@@ -75,18 +76,23 @@ public class Dijkstra {
      */
     int oneStep() {
         int x = nextNode();
+        if(x==-1){
+            return x;
+        }
         settled[x] = true; // Marce-o como acomodado
 
         // Pressione Ctrl e clique na função successors() para ver sua implementação
         ListIterator<Integer> it = g.successors(x).listIterator(); // g.successors(i) retorna um ArrayList dos sucessores de i;
 
         /**
-         * Para cada sucessor v de x: Atualize(v, x); fimPara
+         * Para cada sucessor v de x: 
+         *  Atualize(v, x); 
+         * fimPara
          */
         while (it.hasNext()) {
             update(it.next(), x);
         }
-        return x;
+        return x; //nó i que acabou de ser acomodado
     }
 
     /**
@@ -97,8 +103,16 @@ public class Dijkstra {
      * @return o valor do menor custo
      */
     int compute() {
-        while(!settled[dest]){
-            oneStep();
+        int menor=oneStep(), aux;
+        do{
+            aux = oneStep();
+            if(aux < menor) menor = aux;
+        }while(!settled[dest] && !naoacomodados.isEmpty());
+        
+        if(menor>-1){
+            return dist[dest];
+        }else{
+            return -1;
         }
     }
 
@@ -114,8 +128,8 @@ public class Dijkstra {
     }
 
     public static void test2() throws Exception {
-        System.out.println("Teste 2 : inicializaÃ§Ã£o da classe Dijkstra");
-        Graph g = new Graph("C:\\Users\\ana\\Documents\\OneDriveVictor\\OneDrive\\UFRN\\Estrutura de Dados\\Unidade 3\\pratica9\\Pratica9\\src\\codigo\\fonte\\mini.gr");
+        System.out.println("Teste 2 : inicialização da classe Dijkstra");
+        Graph g = new Graph("C:\\Users\\user41\\Desktop\\DCA0204-EstruturaDeDados\\Unidade 3\\Pratica9\\src\\codigo\\fonte\\mini.gr");
         Dijkstra d = new Dijkstra(g, 2, 7);
         int n = g.n;
         System.out.println("---Grafo---");
@@ -128,11 +142,47 @@ public class Dijkstra {
         }
     }
 
-    public static void test3() {
+    public static void test3() throws Exception {
+        System.out.println("Teste 3 : teste do algoritmo de Dijkstra");
 
+        // petit graphe
+        System.out.println("\ngrafo pequeno");
+        Graph g = new Graph("C:\\Users\\user41\\Desktop\\DCA0204-EstruturaDeDados\\Unidade 3\\Pratica9\\src\\codigo\\fonte\\mini.gr");
+        Dijkstra d = new Dijkstra(g, 2, 7);
+        System.out.println("caminho mais curto entre 2 e 7 = " + d.compute());
+        d = new Dijkstra(g, 7, 2);
+        System.out.println("caminho mais curto entre 7 e 2 = " + d.compute());
+
+        // gros graphe
+        System.out.println("\ngrafo grande");
+        g = new Graph("C:\\Users\\user41\\Desktop\\DCA0204-EstruturaDeDados\\Unidade 3\\Pratica9\\src\\codigo\\fonte\\USA-road-d-NY.gr");
+        d = new Dijkstra(g, 190637, 187333);
+        System.out.println("caminho mais curto entre 190637 e 187333 = " + d.compute());
     }
 
-    public static void main(String[] args) {
+    public static void test4() throws Exception{
+        System.out.println("Teste 4 : teste do algoritmo de Dijkstra");
 
+        // preparation de la fenetre
+        Graph g = new Graph("C:\\Users\\user41\\Desktop\\DCA0204-EstruturaDeDados\\Unidade 3\\Pratica9\\src\\codigo\\fonte\\USA-road-d-NY.gr");
+        g.setCoordinates("C:\\Users\\user41\\Desktop\\DCA0204-EstruturaDeDados\\Unidade 3\\Pratica9\\src\\codigo\\fonte\\USA-road-d-NY.co");
+        Fenetre f;
+        f = new Fenetre("C:\\Users\\user41\\Desktop\\DCA0204-EstruturaDeDados\\Unidade 3\\Pratica9\\src\\codigo\\fonte\\NY_Metropolitan.png", "Dijkstra", -73.9987, -73.9437, 40.7523, 40.78085);
+        g.drawGraph(f);
+        f.repaint();
+
+        // algorithme de Dijkstra
+        int source = 190637, destination = 187333;
+        Dijkstra d = new Dijkstra(g, source, destination);
+        d.f = f;
+        System.out.println("caminho mais curto entre " + source + " et " + destination + " = " + d.compute());
+        g.drawSourceDestination(f, source, destination);
+        g.drawPath(f, d.pred, destination);
+    }
+
+    public static void main(String[] args) throws Exception {
+        test2();
+        test3();
+        test4();
     }
 }
